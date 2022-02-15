24694c03daf8ab4ffeda4af2806b5f798e99b30e
sydyk120276 <akbar@github.com>
2022-02-15 14:14:26 +0600

    вывод массива координат на карту

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React from "react";
 
 import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -7,45 +7,6 @@ import placesData from '../../data/data.json'
 
 
 const Maps = () => {
-  const [state, setState] = useState("");
-  console.log("state: ", state);
-  //  const array = placesData.Point.pos.split(" ").map((el) => Number(el));
-  let aaa = Number(placesData.shirota)
-  let bbb = Number(placesData.dolgota)
-
-  console.log("placesData: ", Number(placesData.shirota))
-  // console.log("array: ", array);
-  // const obj = {
-  //   latitude: array[0],
-  //   longitude: array[1],
-  // };
-  // console.log("obj: ", obj);
-
-  // const arrayObj = [...[placesData], obj]
-  // console.log("arrayObj: ", arrayObj);
-
-  // let adress = [
-  //   "Бишкек,+ибраимова+улица,+дом+181",
-  //   "Бишкек,+Крылова+улица,+дом+5",
-  //   "Бишкек,+Ю.Фучика+улица,+дом+15",
-  //   "Бишкек,+Тоголока Молдо+улица,+дом+3",
-  // ];
-
-  // useEffect(() => {
-  //   adress.map((el) => {
-  //     return axios(
-  //       `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=8201c1c3-1913-411b-9300-e3de7614b6f7&geocode=${el}`
-  //     )
-  //       .then(({ data }) => {
-
-  //         return setState(
-  //           data.response.GeoObjectCollection.featureMember[1].GeoObject.Point
-  //             .pos
-  //         );
-  //       })
-  //       .catch((err) => err);
-  //   });
-  // }, []);
 
   let myIcon = L.icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/4478/4478094.png",
@@ -59,12 +20,15 @@ const Maps = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker
-          position={[Number(placesData.shirota), Number(placesData.dolgota)]}
-          icon={myIcon}
-        >
-          <Popup>{placesData.AddressLine}</Popup>
-        </Marker>
+        {placesData.map((elem) => (
+          <Marker
+            key={elem.shirota}
+            position={[Number(elem.shirota), Number(elem.dolgota)]}
+            icon={myIcon}
+          >
+            <Popup>{elem.AddressLine}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,47 +1,43 @@
-import express from "express"
-import { resolve } from "path"
-import cors from 'cors'
-import cookieParser from "cookie-parser"
-import axios from "axios"
-import { writeFile } from "fs/promises"
+import express from "express";
+import { resolve } from "path";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import axios from "axios";
+import { writeFile } from "fs/promises";
 
-import { Html } from "../client/html.js"
+import { Html } from "../client/html.js";
 
+const server = express();
 
-
-const server = express()
-
-const PORT = process.env.PORT || 8080
-const __dirname = process.cwd()
+const PORT = process.env.PORT || 8080;
+const __dirname = process.cwd();
 
 const middleware = [
   cors(),
   cookieParser(),
   express.json({ limit: "40kb" }),
-  express.static(resolve(__dirname, "dist"))
-]
+  express.static(resolve(__dirname, "dist")),
+];
 
-middleware.forEach((it) => server.use(it))
+middleware.forEach((it) => server.use(it));
 
-server.get('/', (req, res) => {
-  res.send('Express Server')
-})
+server.get("/", (req, res) => {
+  res.send("Express Server");
+});
 
-  let adress = [
-    "Бишкек,+ибраимова+улица,+дом+181",
-    "Бишкек,+Крылова+улица,+дом+5",
-    "Бишкек,+Ю.Фучика+улица,+дом+15",
-    "Бишкек,+Тоголока Молдо+улица,+дом+3"
-  ]
+let adress = [
+  "Бишкек,+ибраимова+улица,+дом+181",
+  "Бишкек,+Крылова+улица,+дом+5",
+  "Бишкек,+Ю.Фучика+улица,+дом+15",
+  "Бишкек,+Тоголока Молдо+улица,+дом+3",
+];
 
-  const url = encodeURI(
-    `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=8201c1c3-1913-411b-9300-e3de7614b6f7&geocode=Бишкек,+Крылова+улица,+дом+5`
-  );
 
-  server.get('/api/v1/adress',async (req, res) => {
-let obj = [];
-    const address = await Promise.all(adress.map(async (elem) => {
-     return await axios(
+server.get("/api/v1/adress", async (req, res) => {
+  let obj = [];
+  await Promise.all(
+    adress.map(async (elem) => {
+      return await axios(
         encodeURI(
           `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=8201c1c3-1913-411b-9300-e3de7614b6f7&geocode=${elem}`
         )
@@ -60,29 +56,28 @@ let obj = [];
           }
           return obj;
         })
-        .catch((err) => err)
+        .catch((err) => err);
+    })
+  );
+  console.log("obj:______", obj);
+  writeFile(`${__dirname}/data/data.json`, JSON.stringify(obj), "utf-8");
 
-    }));
-      console.log("obj:______", obj);
-      writeFile(`${__dirname}/data/data.json`, JSON.stringify(obj), "utf-8");
-
-            console.log("address:____", address)
-     res.json(address)
-  })
-
+  console.log("address:____", obj);
+  res.json(obj);
+});
 
 server.get("/*", (req, res) => {
   const initialState = {
-    location: req.url
-  }
+    location: req.url,
+  };
   res.send(
     Html({
-      body: '',
-      initialState
+      body: "",
+      initialState,
     })
-  )
-})
+  );
+});
 
 server.listen(PORT, () => {
-  console.log(`Serving at http://localhost${PORT}`)
-})
+  console.log(`Serving at http://localhost${PORT}`);
+});
